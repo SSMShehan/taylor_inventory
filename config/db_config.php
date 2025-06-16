aefdc987e4277dc67147909fc93a38bdff90cb14
Blitz AS <It23839656@my.sliit.lk>
2025-06-16 21:04:19 +0530

    db change

--- config/db_config.php
+++ config/db_config.php
@@ -3,7 +3,7 @@
 $db_server = "localhost";
 $db_user = "root";
 $db_pass = "";
-$db_name = "taylor_inventory";
+$db_name = "ims db";
 
 $conn = new mysqli($db_server, $db_user, $db_pass, $db_name); // Create connection
 
